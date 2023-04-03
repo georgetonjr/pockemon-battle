191f52ee80cc22a965dae557f078083b0b5d412c
Georgeton Junior <georgetonjr@gmail.com>
2023-04-03 15:35:14 -0300

    fix: fix request validator

--- src/main/middleware/update-pokemon-validator-middleware.ts
+++ src/main/middleware/update-pokemon-validator-middleware.ts
@@ -4,7 +4,7 @@ import Joi from 'joi';
 
 export const UpdatePokemonValidator = (req: Request, res: Response, next: NextFunction) => {
   const updatePokemonSchema = Joi.object<UpdatePokemonRequest>().keys({
-    treinador: Joi.string().valid('charizard', 'mewtwo', 'pikachu').required(),
+    treinador: Joi.string().required(),
   });
   const { error } = updatePokemonSchema.validate(req.body)  
   if (error) {
